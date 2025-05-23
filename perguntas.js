6fc06d65ac1c6083cb0f8d4473b7a3c16cd7400d
palestraitalia <141340393+palestraitalia@users.noreply.github.com>
2025-05-23 07:50:46 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,36 @@
+criaCartao(
+    'quizz',
+    'Qual a escala de trabalho do Neymar?',
+    '364x1 e em ano bisexto: 365x1'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Criptomoeda?',
+    'Uma moeda virtual'
+)
+
+criaCartao(
+    'Quizz',
+    'O Pelé tem quantas copas do mundo?',
+    'Ele tem 3'
+)
+
+criaCartao(
+    'Quizz Santos FC.',
+    'Quantas libertadores o Santos FC tem?',
+    '3 libertadores'
+)
+
+
+criaCartao(
+    'Programação',
+    'Qual a finalidade da variável em javascript?',
+    'Armazenar valores aleatórios a serem transcritos no html'
+)
